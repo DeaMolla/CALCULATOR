04841093dc6d00295fa33f5792de1959bf7bd593
DeaMolla <128810179+DeaMolla@users.noreply.github.com>
2023-03-28 00:38:21 -0700

    init

---
+++ calculator1.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+float Summation ()
+{
+    float x,y;
+    printf("What numbers do u want to add?");
+    scanf("%f %f ",&x,&y);
+    return x+y;
+}
+float Substraction()
+{
+    float x,y;
+    printf("What numbers do u want to substract?");
+    scanf("%f %f ",&x,&y);
+    return x-y;
+}
+float Multiplication()
+{
+    float x,y;
+    printf("What numbers do u want to multiply?");
+    scanf("%f %f ",&x,&y);
+    return x*y;
+}
+float Division()
+{
+    float x,y;
+    printf("What numbers do u want to divide?");
+    scanf("%f %f" ,&x,&y);
+    return x/y;
+}
+float SQRT ()
+{
+    float square,x;
+
+    printf("Which number do you want to find the root of?");
+    scanf("%f ",&x);
+    square=sqrt(x);
+    return square;
+}
+float POWER()
+{
+    float power,x;
+    int m;
+     printf("What is the base and the exponent?");
+    scanf("%f %d ",&x,&m);
+    power=pow(x, m);
+    return power;
+}
+int main()
+{
+    //Welcome to my first lines of this code.
+
+    char sign;
+    printf("What operation do you want to do?\n");
+printf("Type + for addition,- for substraction,* for multiplication ,/ for division,^ for power and s for square root");
+scanf("%c",&sign);
+if(sign=='+'){
+ printf("%.3f",Summation());
+}
+else if(sign=='-')
+{
+     printf("%.3f",Substraction());
+}
+else if(sign=='*')
+{
+     printf("%.3f",Multiplication());
+}
+else if(sign=='/')
+{
+     printf("%.3f",Division());
+}
+else if(sign=='^')
+{
+     printf("%.3f",POWER());
+}
+else if(sign=='s')
+{
+     printf("%.3f",SQRT());
+}
+}
