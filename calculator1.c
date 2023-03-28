0ff3ff61a7be4defcfa49f0b33c039ec3dcffdb1
DeaMolla <128810179+DeaMolla@users.noreply.github.com>
2023-03-28 01:35:27 -0700

    average

--- calculator1.c
+++ calculator1.c
@@ -6,21 +6,21 @@ float Summation ()
 {
     float x,y;
     printf("What numbers do u want to add?");
-    scanf("%f %f ",&x,&y);
+    scanf("%f %f",&x,&y);
     return x+y;
 }
 float Substraction()
 {
     float x,y;
     printf("What numbers do u want to substract?");
-    scanf("%f %f ",&x,&y);
+    scanf("%f %f",&x,&y);
     return x-y;
 }
 float Multiplication()
 {
     float x,y;
     printf("What numbers do u want to multiply?");
-    scanf("%f %f ",&x,&y);
+    scanf("%f %f",&x,&y);
     return x*y;
 }
 float Division()
@@ -35,7 +35,7 @@ float SQRT ()
     float square,x;
 
     printf("Which number do you want to find the root of?");
-    scanf("%f ",&x);
+    scanf("%f",&x);
     square=sqrt(x);
     return square;
 }
@@ -44,9 +44,22 @@ float POWER()
     float power,x;
     int m;
      printf("What is the base and the exponent?");
-    scanf("%f %d ",&x,&m);
+    scanf("%f %d",&x,&m);
     power=pow(x, m);
     return power;
+}
+float Average()
+{
+    float number,sum;
+    int n;
+    printf("How many numbers do u want to find the average of?");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%f",&number);
+        sum+=number;
+    }
+    return sum/n;
 }
 int main()
 {
@@ -54,7 +67,7 @@ int main()
 
     char sign;
     printf("What operation do you want to do?\n");
-printf("Type + for addition,- for substraction,* for multiplication ,/ for division,^ for power and s for square root");
+printf("Type + for addition,- for substraction,* for multiplication ,/ for division,^ for power and s for square root and a for average");
 scanf("%c",&sign);
 if(sign=='+'){
  printf("%.3f",Summation());
@@ -78,5 +91,9 @@ else if(sign=='^')
 else if(sign=='s')
 {
      printf("%.3f",SQRT());
+}
+else if(sign=='a')
+{
+    printf("%.3f",Average());
 }
 }
